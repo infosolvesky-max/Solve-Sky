89a0c30be13a40b3e8b4cbd4b1053e509582475b
infosolvesky-max <info.solvesky@gmail.com>
2025-10-28 00:41:28 -0700

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,14 +20,14 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.solvesky_app"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+
+        // Multidex চালু করার জন্য এই লাইনটি বিশেষভাবে যোগ করা হলো
+        multiDexEnabled = true
     }
 
     buildTypes {
