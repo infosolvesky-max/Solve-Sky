0c3addf5cbaabc1282e5bf85d6803e41e055cc51
infosolvesky-max <info.solvesky@gmail.com>
2025-10-28 02:50:54 -0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,12 +1,12 @@
 // android/build.gradle.kts ফাইলের জন্য চূড়ান্ত এবং নির্ভুল কোড
 
 plugins {
-    // Codemagic-এর বিল্ড পরিবেশের সাথে মিলিয়ে সংস্করণ 8.0.1 করা হলো
-    id("com.android.application") version "8.0.1" apply false
+    // Codemagic-এর ত্রুটি অনুযায়ী, নিশ্চিতভাবে 8.0.1 ব্যবহার করা হলো
+    id("com.android.application") version "8.0.1" apply false 
     
-    // সামঞ্জস্যপূর্ণ কোটলিন সংস্করণ যোগ করা হলো
+    // সামঞ্জস্যপূর্ণ কোটলিন সংস্করণ
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     
-    // Flutter প্লাগইনের সংস্করণ নির্দিষ্ট করা হলো
+    // Flutter প্লাগইনের সংস্করণ
     id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
 }
