2cc5148dcc231d73dd3e2b3bc2343f5f6f730205
infosolvesky-max <info.solvesky@gmail.com>
2025-10-28 02:19:06 -0700

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,12 +1,24 @@
-// android/build.gradle.kts ফাইলের জন্য চূড়ান্ত এবং নির্ভুল কোড
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 
-plugins {
-    // Codemagic-এর বিল্ড পরিবেশের সাথে মিলিয়ে সংস্করণ 8.0.1 করা হলো
-    id("com.android.application") version "8.0.1" apply false
-    
-    // সামঞ্জস্যপূর্ণ কোটলিন সংস্করণ যোগ করা হলো
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-    
-    // Flutter প্লাগইনের সংস্করণ নির্দিষ্ট করা হলো
-    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
+val newBuildDir: Directory =
+    rootProject.layout.buildDirectory
+        .dir("../../build")
+        .get()
+rootProject.layout.buildDirectory.value(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.value(newSubprojectBuildDir)
+}
+subprojects {
+    project.evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
